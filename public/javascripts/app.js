ede9f002644a393bdb8e05c437fef7a1085da485
Simon Johansson <mail@simon-johansson.com>
2016-09-21 21:36:38 +0200

    Add framer

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,517 @@
-// Welcome to Framer
+var Input, TextLayer, bg, card, current, input, orgPeeps, overlay, peeps, point, showNewPerson,
+  extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+  hasProp = {}.hasOwnProperty;
 
-// This is just demo code. Feel free to delete it all.
+Framer.Info = {
+  title: "",
+  author: "Andreas Wahlström",
+  twitter: "",
+  description: ""
+};
 
-imageLayer = new Layer({x:0, y:0, width:128, height:128, image:"images/Icon.png"})
-imageLayer.center()
+TextLayer = (function(superClass) {
+  extend(TextLayer, superClass);
 
-print(people)
+  function TextLayer(options) {
+    if (options == null) {
+      options = {};
+    }
+    this.doAutoSize = false;
+    this.doAutoSizeHeight = false;
+    if (options.backgroundColor == null) {
+      options.backgroundColor = options.setup ? "hsla(60, 90%, 47%, .4)" : "transparent";
+    }
+    if (options.color == null) {
+      options.color = "red";
+    }
+    if (options.lineHeight == null) {
+      options.lineHeight = 1.25;
+    }
+    if (options.fontFamily == null) {
+      options.fontFamily = "Helvetica";
+    }
+    if (options.fontSize == null) {
+      options.fontSize = 20;
+    }
+    if (options.text == null) {
+      options.text = "Use layer.text to add text";
+    }
+    TextLayer.__super__.constructor.call(this, options);
+    this.style.whiteSpace = "pre-line";
+    this.style.outline = "none";
+  }
+
+  TextLayer.prototype.setStyle = function(property, value, pxSuffix) {
+    if (pxSuffix == null) {
+      pxSuffix = false;
+    }
+    this.style[property] = pxSuffix ? value + "px" : value;
+    this.emit("change:" + property, value);
+    if (this.doAutoSize) {
+      return this.calcSize();
+    }
+  };
+
+  TextLayer.prototype.calcSize = function() {
+    var constraints, size, sizeAffectingStyles;
+    sizeAffectingStyles = {
+      lineHeight: this.style["line-height"],
+      fontSize: this.style["font-size"],
+      fontWeight: this.style["font-weight"],
+      paddingTop: this.style["padding-top"],
+      paddingRight: this.style["padding-right"],
+      paddingBottom: this.style["padding-bottom"],
+      paddingLeft: this.style["padding-left"],
+      textTransform: this.style["text-transform"],
+      borderWidth: this.style["border-width"],
+      letterSpacing: this.style["letter-spacing"],
+      fontFamily: this.style["font-family"],
+      fontStyle: this.style["font-style"],
+      fontVariant: this.style["font-variant"]
+    };
+    constraints = {};
+    if (this.doAutoSizeHeight) {
+      constraints.width = this.width;
+    }
+    size = Utils.textSize(this.text, sizeAffectingStyles, constraints);
+    if (this.style.textAlign === "right") {
+      this.width = size.width;
+      this.x = this.x - this.width;
+    } else {
+      this.width = size.width;
+    }
+    return this.height = size.height;
+  };
+
+  TextLayer.define("autoSize", {
+    get: function() {
+      return this.doAutoSize;
+    },
+    set: function(value) {
+      this.doAutoSize = value;
+      if (this.doAutoSize) {
+        return this.calcSize();
+      }
+    }
+  });
+
+  TextLayer.define("autoSizeHeight", {
+    set: function(value) {
+      this.doAutoSize = value;
+      this.doAutoSizeHeight = value;
+      if (this.doAutoSize) {
+        return this.calcSize();
+      }
+    }
+  });
+
+  TextLayer.define("contentEditable", {
+    set: function(boolean) {
+      this._element.contentEditable = boolean;
+      this.ignoreEvents = !boolean;
+      return this.on("input", function() {
+        if (this.doAutoSize) {
+          return this.calcSize();
+        }
+      });
+    }
+  });
+
+  TextLayer.define("text", {
+    get: function() {
+      return this._element.textContent;
+    },
+    set: function(value) {
+      this._element.textContent = value;
+      this.emit("change:text", value);
+      if (this.doAutoSize) {
+        return this.calcSize();
+      }
+    }
+  });
+
+  TextLayer.define("fontFamily", {
+    get: function() {
+      return this.style.fontFamily;
+    },
+    set: function(value) {
+      return this.setStyle("fontFamily", value);
+    }
+  });
+
+  TextLayer.define("fontSize", {
+    get: function() {
+      return this.style.fontSize.replace("px", "");
+    },
+    set: function(value) {
+      return this.setStyle("fontSize", value, true);
+    }
+  });
+
+  TextLayer.define("lineHeight", {
+    get: function() {
+      return this.style.lineHeight;
+    },
+    set: function(value) {
+      return this.setStyle("lineHeight", value);
+    }
+  });
+
+  TextLayer.define("fontWeight", {
+    get: function() {
+      return this.style.fontWeight;
+    },
+    set: function(value) {
+      return this.setStyle("fontWeight", value);
+    }
+  });
+
+  TextLayer.define("fontStyle", {
+    get: function() {
+      return this.style.fontStyle;
+    },
+    set: function(value) {
+      return this.setStyle("fontStyle", value);
+    }
+  });
+
+  TextLayer.define("fontVariant", {
+    get: function() {
+      return this.style.fontVariant;
+    },
+    set: function(value) {
+      return this.setStyle("fontVariant", value);
+    }
+  });
+
+  TextLayer.define("padding", {
+    set: function(value) {
+      this.setStyle("paddingTop", value, true);
+      this.setStyle("paddingRight", value, true);
+      this.setStyle("paddingBottom", value, true);
+      return this.setStyle("paddingLeft", value, true);
+    }
+  });
+
+  TextLayer.define("paddingTop", {
+    get: function() {
+      return this.style.paddingTop.replace("px", "");
+    },
+    set: function(value) {
+      return this.setStyle("paddingTop", value, true);
+    }
+  });
+
+  TextLayer.define("paddingRight", {
+    get: function() {
+      return this.style.paddingRight.replace("px", "");
+    },
+    set: function(value) {
+      return this.setStyle("paddingRight", value, true);
+    }
+  });
+
+  TextLayer.define("paddingBottom", {
+    get: function() {
+      return this.style.paddingBottom.replace("px", "");
+    },
+    set: function(value) {
+      return this.setStyle("paddingBottom", value, true);
+    }
+  });
+
+  TextLayer.define("paddingLeft", {
+    get: function() {
+      return this.style.paddingLeft.replace("px", "");
+    },
+    set: function(value) {
+      return this.setStyle("paddingLeft", value, true);
+    }
+  });
+
+  TextLayer.define("textAlign", {
+    set: function(value) {
+      return this.setStyle("textAlign", value);
+    }
+  });
+
+  TextLayer.define("textTransform", {
+    get: function() {
+      return this.style.textTransform;
+    },
+    set: function(value) {
+      return this.setStyle("textTransform", value);
+    }
+  });
+
+  TextLayer.define("letterSpacing", {
+    get: function() {
+      return this.style.letterSpacing.replace("px", "");
+    },
+    set: function(value) {
+      return this.setStyle("letterSpacing", value, true);
+    }
+  });
+
+  TextLayer.define("length", {
+    get: function() {
+      return this.text.length;
+    }
+  });
+
+  return TextLayer;
+
+})(Layer);
+
+TextLayer = TextLayer;
+
+Input = (function(superClass) {
+  extend(Input, superClass);
+
+  Input.define("style", {
+    get: function() {
+      return this.input.style;
+    },
+    set: function(value) {
+      return _.extend(this.input.style, value);
+    }
+  });
+
+  Input.define("value", {
+    get: function() {
+      return this.input.value;
+    },
+    set: function(value) {
+      return this.input.value = value;
+    }
+  });
+
+  function Input(options) {
+    if (options == null) {
+      options = {};
+    }
+    if (options.setup == null) {
+      options.setup = false;
+    }
+    if (options.width == null) {
+      options.width = Screen.width;
+    }
+    if (options.clip == null) {
+      options.clip = false;
+    }
+    if (options.height == null) {
+      options.height = 60;
+    }
+    if (options.backgroundColor == null) {
+      options.backgroundColor = options.setup ? "rgba(255, 60, 47, .5)" : "transparent";
+    }
+    if (options.fontSize == null) {
+      options.fontSize = 30;
+    }
+    if (options.lineHeight == null) {
+      options.lineHeight = 30;
+    }
+    if (options.padding == null) {
+      options.padding = 10;
+    }
+    if (options.text == null) {
+      options.text = "";
+    }
+    if (options.placeholder == null) {
+      options.placeholder = "";
+    }
+    if (options.virtualKeyboard == null) {
+      options.virtualKeyboard = Utils.isMobile() ? false : true;
+    }
+    if (options.type == null) {
+      options.type = "text";
+    }
+    if (options.goButton == null) {
+      options.goButton = false;
+    }
+    Input.__super__.constructor.call(this, options);
+    if (options.placeholderColor != null) {
+      this.placeholderColor = options.placeholderColor;
+    }
+    this.input = document.createElement("input");
+    this.input.id = "input-" + (_.now());
+    this.input.style.cssText = "font-size: " + options.fontSize + "px; line-height: " + options.lineHeight + "px; padding: " + options.padding + "px; width: " + options.width + "px; height: " + options.height + "px; border: none; outline-width: 0; background-image: url(about:blank); background-color: " + options.backgroundColor + ";";
+    this.input.value = options.text;
+    this.input.type = options.type;
+    this.input.placeholder = options.placeholder;
+    this.form = document.createElement("form");
+    if (options.goButton) {
+      this.form.action = "#";
+      this.form.addEventListener("submit", function(event) {
+        return event.preventDefault();
+      });
+    }
+    this.form.appendChild(this.input);
+    this._element.appendChild(this.form);
+    this.backgroundColor = "transparent";
+    if (this.placeholderColor) {
+      this.updatePlaceholderColor(options.placeholderColor);
+    }
+  }
+
+  Input.prototype.updatePlaceholderColor = function(color) {
+    var css;
+    this.placeholderColor = color;
+    if (this.pageStyle != null) {
+      document.head.removeChild(this.pageStyle);
+    }
+    this.pageStyle = document.createElement("style");
+    this.pageStyle.type = "text/css";
+    css = "#" + this.input.id + "::-webkit-input-placeholder { color: " + this.placeholderColor + "; }";
+    this.pageStyle.appendChild(document.createTextNode(css));
+    return document.head.appendChild(this.pageStyle);
+  };
+
+  Input.prototype.focus = function() {
+    return this.input.focus();
+  };
+
+  Input.prototype.onFocus = function(cb) {
+    return this.input.addEventListener("focus", function() {
+      return cb.apply(this);
+    });
+  };
+
+  Input.prototype.onBlur = function(cb) {
+    return this.input.addEventListener("blur", function() {
+      return cb.apply(this);
+    });
+  };
+
+  return Input;
+
+})(Layer);
+
+bg = new BackgroundLayer({
+  backgroundColor: "#D11858"
+});
+
+// peeps = [
+//   {
+//     name: "Small small",
+//     role: "role1",
+//     img: "http://fillmurray.com/10/10"
+//   }, {
+//     name: "medium medium",
+//     role: "role2",
+//     img: "http://fillmurray.com/50/50"
+//   }, {
+//     name: "big big",
+//     role: "role3",
+//     img: "http://fillmurray.com/300/300"
+//   }
+// ];
+
+orgPeeps = peeps;
+
+current = null;
+
+point = new TextLayer({
+  fontSize: 60,
+  y: 100,
+  text: 0,
+  autoSize: true,
+  color: "white"
+});
+
+point.centerX();
+
+card = new Layer({
+  width: 300,
+  height: 300,
+  midY: Screen.midY
+});
+
+showNewPerson = function() {
+  if (peeps.length === 0) {
+    overlay.visible = true;
+    overlay.text = "Perfect score! " + point.text + "/" + peeps.length + "\n\nClick to play again";
+    return;
+  }
+  card.x = -600;
+  card.rotation = -10;
+  current = Utils.randomChoice(peeps);
+  card.image = current.img;
+  card.scale = 1;
+  return card.animate({
+    properties: {
+      scale: 1,
+      midX: Screen.midX,
+      midY: Screen.midY,
+      rotation: 0
+    },
+    curve: "spring(200,20)",
+    delay: .2
+  });
+};
+
+input = new Input({
+  placeholder: "Vad heter kollegan?",
+  placeholderColor: "grey",
+  backgroundColor: "white",
+  type: "text",
+  maxY: Screen.height,
+  width: Screen.width,
+  height: 100,
+  fontSize: 30
+});
+
+input.style = {
+  "padding": "0 0 0px 20px"
+};
+
+showNewPerson();
+
+input.form.addEventListener("submit", function(e) {
+  var out;
+  e.preventDefault();
+  if (input.value.toLowerCase() === current.name.toLowerCase() || input.value.toLowerCase() === current.name.split(' ')[0].toLowerCase()) {
+    point.text = ++point.text;
+    point.scale = 0.5;
+    point.animate({
+      properties: {
+        scale: 1
+      },
+      curve: "spring(200,10)"
+    });
+    peeps = _.without(peeps, current);
+    out = card.animate({
+      properties: {
+        x: Screen.width + 100
+      },
+      curve: "spring(200,10)"
+    });
+    out.onAnimationEnd(function() {
+      return showNewPerson();
+    });
+  } else {
+    overlay.text = ':( \n\n' + point.text + "/" + peeps.length + "\n\n Click to play again";
+    overlay.visible = true;
+    point.text = 0;
+    peeps = orgPeeps;
+    showNewPerson();
+  }
+  return input.value = "";
+});
+
+overlay = new TextLayer({
+  backgroundColor: "white",
+  paddingTop: 100,
+  paddingLeft: 50,
+  color: "#D11858",
+  size: Screen.size,
+  fontSize: 60,
+  visible: false
+});
+
+overlay.onClick(function() {
+  return window.location.href = window.location.href;
+});
+
+// ---
+// generated by coffee-script 1.9.2
